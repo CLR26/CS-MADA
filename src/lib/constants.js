fc58e5a3ba2489f94c07a0c50fdaa8168d842f00
CLR26 <bpo1@celerogroup.com>
2026-08-25 10:58:36 +0300

    Add files via upload

---
+++ src/lib/constants.js
@@ -0,0 +1,7 @@
+/**
+ * Valeurs métier normalisées, partagées entre les formulaires et les filtres.
+ * Toute nouvelle valeur doit être ajoutée ici uniquement (source unique de vérité)
+ * pour garantir des filtres fiables dans le Dashboard.
+ */
+
+export const DEPARTMENTS = ['FinanceOps', 'GroundOps', 'Customer Support']
